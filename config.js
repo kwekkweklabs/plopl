a7ea4e70832e5c26385fb5c554c40757a4c227b4
Kelvin Adithya <72134234+kelvinkn17@users.noreply.github.com>
2025-04-06 09:26:34 +0800

    contract

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ export const CHAINS = [
     name: 'Polygon Amoy',
     wss: `wss://polygon-amoy.nodit.io/${process.env.NODIT_API_KEY}`,
     rpc: `https://polygon-amoy.nodit.io/${process.env.NODIT_API_KEY}`,
-    managerContract: '',
-    registryContract: '',
+    managerContract: '0xb03A1229B8B71cD5C97Abd10BE0238700970a770',
+    registryContract: '0x5c6d990Faef0a400724e67d7c8A31E9AC25eD579',
   },
   {
     id: 48899,
     name: `Zircuit Testnet`,
     wss: `https://testnet.zircuit.co`,
     rpc: `https://testnet.zircuit.co`,
-    managerContract: '',
-    registryContract: '',
+    managerContract: '0x6527800Ef9f9c0772e064F5302592A354b1D07Cc',
+    registryContract: '0x0eA5714bb9fbe8E6031821173055fae04ba783Cb',
   }
 ]
